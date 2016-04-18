f69d485c3196e56604b02856ba12ec6bc28b2b1d
ClassCat <support@classcat.com>
2016-04-18 11:05:48 +0900

    ccdl-05

--- scripts/ccdl-05_build_bazel.sh
+++ scripts/ccdl-05_build_bazel.sh
@@ -6,6 +6,9 @@
 ###################################################################
 
 # --- HISTORY -----------------------------------------------------
+# 18-apr-16 : alpha.
+#
+# --- 071 ---------------------------------------------------------
 # 22-mar-16 : rc 0xff.
 # 08-mar-16 : beta 3.
 # 07-mar-16 : beta 2 fixed.
@@ -37,7 +40,7 @@ function show_banner () {
   echo -e  "\tClassCat(R) Deep Learning Service"
   echo -e  "\tCopyright (C) 2016 ClassCat Co.,Ltd. All rights reserved."
   echo -en "\x1b[m"
-  echo -e  "\t\t\x1b[22;34m@Build Bazel\x1b[m: release: rc 0xff (03/22/2016)"
+  echo -e  "\t\t\x1b[22;34m@Build Bazel\x1b[m: release: alpha (04/18/2016)"
   # echo -e  ""
 }
 
@@ -119,7 +122,7 @@ rm -rf bazel
 echo ""
 echo "###################################################"
 echo "# Script execution has been completed successfully."
-echo "# Then, run cctf-06_bro.sh"
+echo "# Then, run ccdl-06_bro.sh"
 echo "###################################################"
 echo ""
 
