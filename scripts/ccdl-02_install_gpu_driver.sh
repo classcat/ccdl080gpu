7364f72961783806e0611aca3736d878a9e13739
ClassCat <support@classcat.com>
2016-04-18 10:53:38 +0900

    ccdl-02

--- scripts/ccdl-02_install_gpu_driver.sh
+++ scripts/ccdl-02_install_gpu_driver.sh
@@ -6,6 +6,9 @@
 ###################################################################
 
 # --- HISTORY -----------------------------------------------------
+# 18-apr-16 : alpha.
+#
+# --- 071 ---------------------------------------------------------
 # 22-mar-16 : rc 0xff.
 # 08-mar-16 : beta 3.
 # 07-mar-16 : beta 2 fixed.
@@ -42,7 +45,7 @@ function show_banner () {
   echo -e  "\tClassCat(R) Deep Learning Service"
   echo -e  "\tCopyright (C) 2016 ClassCat Co.,Ltd. All rights reserved."
   echo -en "\x1b[m"
-  echo -e  "\t\t\x1b[22;34m@Install GPU\x1b[m: release: rc 0xff (03/22/2016)"
+  echo -e  "\t\t\x1b[22;34m@Install GPU\x1b[m: release: alpha (04/18/2016)"
   # echo -e  ""
 }
 
@@ -89,6 +92,7 @@ function install_drm_driver () {
     exit 1
   fi
 }
+
 
 
 ###
@@ -144,7 +148,7 @@ echo "#"
 echo "# 1) To enable DRM/NVIDIA driver, please reboot the instance :"
 echo "#        # sync && reboot "
 echo "#"
-echo "# 2) Then, run cctf-03_install_cuda_and_cudnn.sh."
+echo "# 2) Then, run ccdl-03_install_cuda_and_cudnn.sh."
 echo "###############################################################"
 echo ""
 
