2c68f5122f298361dd2996a122194689aa9a5022
ClassCat <support@classcat.com>
2016-05-10 07:34:59 +0900

    beta

--- scripts_for_guest/tf-02_construct_venv2.sh
+++ scripts_for_guest/tf-02_construct_venv2.sh
@@ -14,7 +14,8 @@
 # o PS1 (01-mar-16)
 #
 # --- HISTORY -----------------------------------------------------
-# 18-apr-16: alpha.
+# 10-may-16 : beta.
+# 18-apr-16 : alpha.
 #
 # --- 071 ---------------------------------------------------------
 # 22-mar-16 : rc 0xff.
@@ -49,7 +50,7 @@ function show_banner () {
   echo -e  "\tClassCat(R) Deep Learning Service"
   echo -e  "\tCopyright (C) 2016 ClassCat Co.,Ltd. All rights reserved."
   echo -en "\x1b[m"
-  echo -e  "\t\t\x1b[22;34m@Construct VirutualEnv 2\x1b[m: release: alpha (04/18/2016)"
+  echo -e  "\t\t\x1b[22;34m@Construct VirutualEnv 2\x1b[m: release: beta (05/10/2016)"
   # echo -e  ""
 }
 
--- scripts_for_guest/tf-03_install_tf080.sh
+++ scripts_for_guest/tf-03_install_tf080.sh
@@ -9,6 +9,7 @@
 # Run on the account - tensorflow070.
 #
 # --- HISTORY -----------------------------------------------------
+# 10-may-16 : beta.
 # 18-apr-16 : alpha.
 #
 # --- 0.7.1 -------------------------------------------------------
@@ -58,7 +59,7 @@ function show_banner () {
   echo -e  "\tClassCat(R) Deep Learning Service"
   echo -e  "\tCopyright (C) 2016 ClassCat Co.,Ltd. All rights reserved."
   echo -en "\x1b[m"
-  echo -e  "\t\t\x1b[22;34m@Insall TensorFlow\x1b[m: release: alpha (04/18/2016)"
+  echo -e  "\t\t\x1b[22;34m@Insall TensorFlow\x1b[m: release: beta (05/10/2016)"
   # echo -e  ""
 }
 
